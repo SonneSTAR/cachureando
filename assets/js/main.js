9b82bac2a105608f1aaaddfc185c7c977e97afc8
AdrianFredes <adrian.fredes.maldonado@gmail.com>
2023-01-13 12:17:42 -0300

    cambios en select

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl);
 })
 
 
-let contenedor = document.getElementById('contenedor')
+let contenedor = document.getElementById('contenedor');
 
 const date = new Date();
 const time = `${date.getHours()}:${date.getMinutes()}`
@@ -14,8 +14,41 @@ const year = date.getFullYear();
 
 const dateModified = `${dateset}/${month}/${year}`;
 
-contenedor.innerHTML = `<p><strong>La fecha actual es ${dateModified} con ${time} minutos<strong/></p>`
+contenedor.innerHTML = `<p><strong>La fecha actual es ${dateModified} con ${time} minutos<strong/></p>`;
+
+
+let evento = document.getElementsByClassName('evento');
+let carrito = document.getElementById('carrito');
+let card1 = document.getElementsByClassName('card-text1').value;
+let calificacion = document.getElementById('calificacion');
+
+calificacion.addEventListener('click', ()=>{
+  console.log('a')
+  if(calificacion >= 0 && calificacion <= 3){
+    console.log(nota)
+    alert('Deficiente')
+  }
+})
+
+evento[0].addEventListener('click', () =>{
+  console.log(card1)
+  carrito.innerHTML = `<tr>
+  <td>${card1}</td>
+  <td>1.000</td>
+  </tr>`
+})
+evento[1].addEventListener('click', () =>{
+  console.log('asd')
+  alert('hola')
+})
+evento[2].addEventListener('click', () =>{
+  console.log('asd')
+  alert('hola')
+})
 
 $(document).ready( function () {
   $('#table_id').DataTable();
-} );
+} ); 
+
+
+
